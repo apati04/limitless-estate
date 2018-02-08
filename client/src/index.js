814347427babb29d4029cf8d2402092e639a5b84
isaac1104 <isaac-kwon@hotmail.com>
2018-02-07 16:32:33 -0800

    navbar with routes added

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import "semantic-ui-css/semantic.min.css";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
