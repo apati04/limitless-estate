56be2b261cb60e2f4f7bfc89cb2df7c4ce779c1d
Andrew Patipaksiri <apax714@gmail.com>
2018-02-15 17:53:47 -0800

    Building for production

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-md navbar-dark bg-dark sticky-top">
       <Link to="/" className="navbar-brand">
-        <img src="http://via.placeholder.com/50x50" className="image-fluid" alt="logo"/> LIMITLESS ESTATES
+        <img src="https://via.placeholder.com/50x50" className="image-fluid" alt="logo"/> LIMITLESS ESTATES
       </Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="mobile-navbar" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
