89719a18a799523fcb27c3eeae0e329f2168e27a
Andrew Patipaksiri <apax714@gmail.com>
2018-02-15 17:42:01 -0800

    Building for production

--- server.js
+++ server.js
@@ -3,19 +3,20 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('client/public'));
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/public/index.html"));
-});
+
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  const path = require("path");
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-  })
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+} else {
+  app.use(express.static('client/public'));
+  app.get("*", function(req, res) {
+    res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
+  });
 }
 
 const PORT = process.env.PORT || 3001;
