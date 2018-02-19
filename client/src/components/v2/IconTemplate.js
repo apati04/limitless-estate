02864bb32982b976bc8f95f1cf77ff2d9306cb28
Andrew Patipaksiri <apax714@gmail.com>
2018-02-19 15:45:06 -0800

    [fix] typo in icon classname

--- client/src/components/v2/IconTemplate.js
+++ client/src/components/v2/IconTemplate.js
@@ -4,7 +4,7 @@ import React from 'react';
 const IconTemplate = ({ icon, title, content }) => {
   return (
     <div className="col-md-4 mb-4 text-center">
-      <i className={`fa fa-${icon} mb-2`}></i>
+      <i className={`fas fa-${icon} mb-2`}></i>
       <h3>{title}</h3>
       <p>{content}</p>
     </div>
