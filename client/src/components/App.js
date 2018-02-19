a98cd5c5996d9fc13e508cfe286da2b8dc3bbe03
Andrew Patipaksiri <apax714@gmail.com>
2018-02-19 00:08:21 -0800

    navbar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
-import Navbar from "./Navbar";
-import Home from "./Home";
-import About from "./About";
-import Services from "./Services";
-import Resources from "./Resources";
-import FAQ from "./FAQ";
-import Contact from "./Contact";
-import Columbus from "./packages/Columbus";
-import Ecorse from "./packages/Ecorse";
+// import Navbar from "./Navbar";
+// import Home from "./Home";
+// import About from "./About";
+// import Services from "./Services";
+// import Resources from "./Resources";
+// import FAQ from "./FAQ";
+// import Contact from "./Contact";
+// import Columbus from "./packages/Columbus";
+// import Ecorse from "./packages/Ecorse";
 import { BrowserRouter, Route } from "react-router-dom";
 
 /* Version 2 */
 import LandingPage from './v2/LandingPage';
+import Navbar from './v2/Navbar-v2';
+
 class App extends Component {
   render() {
     return (
@@ -19,13 +21,13 @@ class App extends Component {
         <div>
           <Navbar/>
             <Route exact path="/" component={LandingPage}/>
-            <Route path="/about" component={About}/>
+            {/* <Route path="/about" component={About}/>
             <Route path="/contact" component={Contact}/>
             <Route path="/faq" component={FAQ}/>
             <Route path="/packages/columbus" component={Columbus}/>
             <Route path="/packages/ecorse" component={Ecorse}/>
             <Route path="/resources" component={Resources}/>
-            <Route path="/services" component={Services}/>
+            <Route path="/services" component={Services}/> */}
         </div>
       </BrowserRouter>
     );
--- client/src/components/v2/Navbar-v2.js
+++ client/src/components/v2/Navbar-v2.js
@@ -1,1 +1,41 @@
-import R
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import MenuItem from './navMenuItem'
+/**
+ * .navbar .nav-link {
+  font-size: 14px;
+  text-transform: uppercase;
+  padding-left: 1em !important;
+  padding-right: 1em !important; }
+
+.navbar .nav-item.active {
+  border-left: #444 3px solid; }
+ */
+class Navbar_v2 extends Component {
+  renderMenu() {
+    const fields = ['home', 'about us', 'markets', 'resources', 'contact'];
+    return fields.map((item) => (
+      <li key={item} className="nav-item">
+        <MenuItem field={item} />
+      </li>
+    ));
+  }
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+        <div className="container">
+          <Link to="/" className="navbar-brand">Limitless Estates, LLC</Link>
+          <button className="navbar-toggler" data-toggle="collapse" data-target="#navNavbar"><span className="navbar-toggler-icon"></span></button>
+          <ul className="navbar-nav ml-auto">
+            {this.renderMenu()}
+          </ul>
+        </div>
+      </nav>
+    )
+  }
+}
+
+export default Navbar_v2;
+
+
+
