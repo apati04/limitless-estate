3cd377b6a2711d12157b7bf96caeccce38da58f9
Andrew Patipaksiri <apax714@gmail.com>
2018-02-20 12:47:02 -0800

    complete page header component

--- client/src/components/PackagePage.js
+++ client/src/components/PackagePage.js
@@ -3,9 +3,12 @@ import PageHeader from './PageHeader';
 
 class PackagePage extends Component {
   render() {
+    const imgURL = 'https://cdn.pixabay.com/photo/2016/10/22/01/54/wood-1759566_960_720.jpg';
+    const description = "optional info"
+    const title = "Packages"
     return (
       <div>
-        <PageHeader title="Packages" />
+        <PageHeader title={title} description={description} img={imgURL} />
       </div>
     )
   }
--- client/src/components/PageHeader.js
+++ client/src/components/PageHeader.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
-const PageHeader = ({title, description}) => {
+
+const PageHeader = ({title, description, img}) => {
+  const style = {
+    pageHeader: {
+      height: '200px',
+      background: `url(${img})`,
+      backgroundPosition: '0 -360px',
+      backgroundAttachment: 'fixed',
+      color: '#fff',
+      borderBottom: '1px #eee solid',
+      paddingTop: '50px'
+    }
+  }
+
   return (
-    <header id="pageHeader">
+    <header style={style.pageHeader} id="pageHeader">
       <div className="container">
         <div className="row">
           <div className="col-md-6 m-auto text-center">
