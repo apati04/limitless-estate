d9cca1ff2416d84ed89f1cef4d5af1be35ed24b9
Andrew Patipaksiri <apax714@gmail.com>
2018-02-19 16:56:43 -0800

    [landing] info section compelete

--- client/src/components/v2/LandingPage.js
+++ client/src/components/v2/LandingPage.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 import SlideShow from './SlideShow';
 import IconSection from './IconSection';
+import LandingParallax from './LandingParallax';
+import InfoSection from './InfoSection';
 class LandingPage extends Component {
   render() {
     return (
       <div>
         <SlideShow />
         <IconSection />
+        <LandingParallax />
+        <InfoSection />
       </div>
     )
   }
