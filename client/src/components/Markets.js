79e71a08f76df1eab0f161adbdd0a25fb6bd781d
Andrew Patipaksiri <apax714@gmail.com>
2018-02-20 21:41:52 -0800

    fix

--- client/src/components/Markets.js
+++ client/src/components/Markets.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { Link } from 'react-router-dom'
 =======
 >>>>>>> packages styling
+=======
+import { Link } from 'react-router-dom'
+>>>>>>> merge fix
 import PageHeader from './PageHeader';
 import PageBox from './PageBox';
 class Markets extends Component {
@@ -17,10 +21,14 @@ class Markets extends Component {
           <div className="row mb-4">
             <div className="col-md-4">
 <<<<<<< HEAD
+<<<<<<< HEAD
                 <Link style={{ textDecoration: 'none' }} to="/markets/columbus"><PageBox icon="building" title="Columbus" bgColor="bg-dark" description="description"/></Link>
 =======
               <PageBox icon="building" title="Columbus" bgColor="bg-primary" description="description" />
 >>>>>>> packages styling
+=======
+                <Link style={{ textDecoration: 'none' }} to="/markets/columbus"><PageBox icon="building" title="Columbus" bgColor="bg-dark" description="description"/></Link>
+>>>>>>> merge fix
             </div>
             <div className="col-md-4">
               <PageBox icon="building" title="Ecorse" bgColor="bg-secondary" description="description" />
--- client/src/components/PageBox.js
+++ client/src/components/PageBox.js
@@ -3,6 +3,7 @@ import React from 'react';
 const PageBox = ({title, description, bgColor, icon}) => {
   return (
 <<<<<<< HEAD
+<<<<<<< HEAD
     <div className={` card ${bgColor} text-center text-white `}>
       <div className="buildBox">
       <div className=" card-body">
@@ -10,15 +11,24 @@ const PageBox = ({title, description, bgColor, icon}) => {
       <div style={{opacity: '0.88'}} className={`card ${bgColor} text-center text-white`}>
         <div className="card-body">
 >>>>>>> packages styling
+=======
+    <div className={` card ${bgColor} text-center text-white `}>
+      <div className="buildBox">
+      <div className=" card-body">
+>>>>>>> merge fix
           <i className={`fa fa-${icon}`}></i>
           <h3>{title}</h3>
           <p className="lead">{description}</p>
         </div>
       </div>
 <<<<<<< HEAD
+<<<<<<< HEAD
       </div>
 =======
 >>>>>>> packages styling
+=======
+      </div>
+>>>>>>> merge fix
   )
 }
 
