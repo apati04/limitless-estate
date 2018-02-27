5992d6e0554f476e8acdab116be10283ddd3f2c7
Andrew Patipaksiri <apax714@gmail.com>
2018-02-26 19:01:47 -0800

    build

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
-const express = require("express");
+const express = require('express');
 const path = require('path');
 
 const app = express();
-
 // Send every request to the React app
 // Define any API routes before this runs
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-  app.get("*", (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 } else {
   app.use(express.static('client/public'));
-  app.get("*", function(req, res) {
+  app.get('*', function(req, res) {
     res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
   });
 }
