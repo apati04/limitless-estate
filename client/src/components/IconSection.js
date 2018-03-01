564921deaa78e23586f731cb4c2d84c1cd55c1d1
isaac1104 <isaac-kwon@hotmail.com>
2018-02-28 22:29:33 -0800

    icon added

--- client/src/components/IconSection.js
+++ client/src/components/IconSection.js
@@ -6,16 +6,9 @@ const IconSection = () => {
     <section id="landingIcons" className="py-5">
       <div className="container">
         <div className="row">
-          <IconTemplate
-            icon='users'
-            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, maxime."
-            title="users"
-          />
-          <IconTemplate
-            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, maxime." title="clock"
-            icon="clock"
-            />
-          <IconTemplate content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat, maxime." icon="chart-line" title="chartline" />
+          <IconTemplate icon='building' fontSize="60px"/>
+          <IconTemplate icon="users" fontSize="60px"/>
+          <IconTemplate icon="dollar-sign" fontSize="60px"/>
         </div>
       </div>
     </section>
--- client/src/components/IconTemplate.js
+++ client/src/components/IconTemplate.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const IconTemplate = ({ icon, title, content }) => {
+const IconTemplate = ({ icon, title, content, fontSize }) => {
   return (
     <div className="col-md-4 mb-4 text-center">
-      <i className={`fas fa-${icon} mb-2`}></i>
+      <i className={`fas fa-${icon} mb-2`} style={{ fontSize: `${fontSize}` }}></i>
       <h3>{title}</h3>
       <p>{content}</p>
     </div>
