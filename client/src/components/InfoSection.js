3b7ddf7de96b525844d6684d43aa690df70a06d7
Andrew Patipaksiri <apax714@gmail.com>
2018-03-03 10:33:54 -0800

    mobile resizing

--- client/src/components/InfoSection.js
+++ client/src/components/InfoSection.js
@@ -6,14 +6,14 @@ const InfoSection = () => {
     <section id="info" className="py-3">
       <div className="container">
         <div className="row">
-          <div style={{ height: '320px' }} className="col-md-6 align-self-center">
+          <div style={{ height: '320px' }} className="col col-md-6 align-self-center">
             <h3>Your Next Chapter Starts With Us!</h3>
             <p>Check out the opportunities that are waiting for you</p>
             <Link to="/markets">
               <button className="btn btn-outline-dark btn-lg mt-5">Sample Deal Packages</button>
             </Link>
           </div>
-          <div className="col-md-6">
+          <div className="col col-md-6">
             <img src="/img/chart2.jpg" className="img-fluid" alt="" />
           </div>
         </div>
