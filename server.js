672871a31040330eb7d5f30645bd7a6ea700fe5a
Andrew Patipaksiri <apax714@gmail.com>
2018-03-03 14:21:35 -0800

    info section position

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('client/build/static'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
