3f6472f573900f826856a2d91807346189d6f8e0
Andrew Patipaksiri <apax714@gmail.com>
2018-03-03 01:47:35 -0800

    fix build

--- client/src/components/LandingParallax.js
+++ client/src/components/LandingParallax.js
@@ -7,7 +7,7 @@ const LandingParallax = () => {
         <div className="row">
           <div className="col">
             <div className="container pt-5">
-              <h1 style={{ marginTop: '8%' }} className="text-white display-4">
+              <h1 style={{ marginTop: '3%' }} className="text-white display-4">
                 Are you ready to invest with us?
               </h1>
               <p className="d-none d-md-block" />
