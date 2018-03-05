5a0cc365b55ee5f53af7c110b3447bbe5b302d60
Andrew Patipaksiri <apax714@gmail.com>
2018-03-05 15:27:16 -0800

    mailer connected

--- server.js
+++ server.js
@@ -1,13 +1,54 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 const app = express();
 // Send every request to the React app
 // Define any API routes before this runs
 app.use(bodyParser.json());
 
 app.post('/api/sendmail', (req, res) => {
-  console.log('body: ', req.body);
+  const { firstname, lastname, email, company, message } = req.body;
+  const poolConfig = {
+    pool: true,
+    host: 'smtp.gmail.com',
+    secure: true,
+  };
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    secure: true,
+    service: 'gmail',
+    auth: {
+      // user: 'aitech9188@gmail.com',
+      // pass: '<uncomment and enter pass>',
+    },
+  });
+
+  const sender = {
+    name: `Inquiry- ${firstname} ${lastname}`,
+    address: email,
+  };
+  const mailOptions = {
+    from: sender,
+    to: 'apax714@gmail.com',
+    subject: `Question- ${firstname}`,
+    html: `
+      <html>
+      <head>
+      </head>
+      <body style="text-align: left;">
+        <h3>New Inquiry from</h3> 
+        <p><strong>First name</strong>: ${firstname}</p>
+        <p><strong>Last name</strong>: ${lastname}</p>
+        <p><strong>Company</strong>: ${company}</p>
+        <p><strong>Email</strong>: ${email}</p>
+        <h3 style="border-bottom: '2px solid grey'; margin-top: 1em;"><strong>message</strong></h3>
+        <p style="margin-top: 0.5em">${message}</p>
+      </body>
+      </html>
+    `,
+  };
+  transporter.sendMail(mailOptions);
   res.send('complete');
 });
 
