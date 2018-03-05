a3c104b9240f82b83727283669629102379e1748
Andrew Patipaksiri <apax714@gmail.com>
2018-03-05 14:57:24 -0800

    exclam

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,16 +1,12 @@
-import React, { Component } from "react";
-import PageHeader from "../components/PageHeader";
-import Form from "../components/Form";
+import React, { Component } from 'react';
+import PageHeader from '../components/PageHeader';
+import Form from '../components/Form';
 
 class Contact extends Component {
   render() {
     return (
       <div>
-        <PageHeader
-          title="Contact Us"
-          description="Feel free to ask us any questions!!!"
-          img="/img/urban.jpg"
-        />
+        <PageHeader title="Contact Us" description="Feel free to ask us any questions!!" img="/img/urban.jpg" />
         <Form />
       </div>
     );
