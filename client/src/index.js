5f1689d690054016143a7f8dc24bb6f83bcc0b69
Andrew Patipaksiri <apax714@gmail.com>
2018-03-09 20:41:54 -0800

    render info icon to screen

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,4 @@
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap-material-design/dist/css/bootstrap-material-design.min.css';
 import 'mapbox-gl/dist/mapbox-gl.css';
@@ -8,5 +9,10 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { unregister } from './registerServiceWorker';
 
+$(function() {
+  $('[data-toggle="popover"]').popover({
+    trigger : 'focus'
+  });
+});
 ReactDOM.render(<App />, document.getElementById('root'));
 unregister();
