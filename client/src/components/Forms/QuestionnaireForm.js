998a85a545dd5d6c80caeced93646b72b192abed
isaac1104 <isaac-kwon@hotmail.com>
2018-03-09 21:15:42 -0800

    forms added

--- client/src/components/Forms/QuestionnaireForm.js
+++ client/src/components/Forms/QuestionnaireForm.js
@@ -36,111 +36,130 @@ class ContactForm extends Component {
         </div>
         <div className="row m-4">
           <div className="col-md-6">
-            <div>
+            <div className="mb-4">
               <label htmlFor="firstname">1. Why are you interested in investing in Real Estate?</label>
               <div className="form-group">
                 <Field className="form-control" type="text" name="question1" />
               </div>
             </div>
-            <div>
+            <div className="mb-4">
               <label htmlFor="lastname">2. What is the biggest hurdle you are trying to overcome by investing in Real Estate?
               </label>
               <div className="form-group">
                 <Field className="form-control" type="text" name="question2" />
               </div>
             </div>
-            <div>
+            <div className="mb-4">
               <label htmlFor="question3">3. Do you want to invest in multifamily, value-add projects?</label>
               <div className="form-group">
-                <Field className="form-control" type="text" name="question3" />
+                <Field type="radio" name="question3" value="Low" className="m-2" />
+                <label htmlFor="question3">Yes</label>
+                <Field type="radio" name="question3" value="Moderate" className="m-2" />
+                <label htmlFor="question3">No</label>
               </div>
             </div>
-            <div>
+            <div className="mb-4">
               <label>4. What are your return expectations?</label>
               <div className="form-group">
-                <ul>
-                  <li>
+                <ul className="list-group">
+                  <li className="list-group-item">
                     <label htmlFor="question4a">Annual return?</label>
                     <Field className="form-control" type="text" name="question4a" />
                   </li>
-                  <li>
+                  <li className="list-group-item">
                     <label htmlFor="question4b">Internal rate of return (IRR)?</label>
                     <Field className="form-control" type="text" name="question4b" />
                   </li>
-                  <li>
+                  <li className="list-group-item">
                     <label htmlFor="question4c">Cash on cash return?</label>
                     <Field className="form-control" type="text" name="question4c" />
                   </li>
                 </ul>
               </div>
             </div>
-            <div>
+            <div className="mb-4">
               <label htmlFor="question5">5. What is your investment minimum and maximum hold time?</label>
               <div className="form-group">
                 <Field className="form-control" type="text" name="question5" />
               </div>
             </div>
-            <div>
+            <div className="mb-4">
               <label htmlFor="question6">6. What is the minimum dollar amount you are willing to invest?</label>
               <div className="form-group">
                 <Field className="form-control" type="text" name="question6" />
               </div>
             </div>
-            <div>
-              <label htmlFor="question7">7. Are you an accredited investor? (Makes more than $200k annually for the past 2 years and will do so in this current year or have a net worth of $1M excluding your primary residence)</label>
-              <div className="form-group">
-                <Field className="form-control" type="text" name="question7" />
-              </div>
-            </div>
           </div>
           <div className="col-md-6">
-            <div>
-              <label htmlFor="question8">8. Can you show proof of funds?</label>
+            <div className="mb-4">
+              <label htmlFor="question7">7. Are you an accredited investor?</label>
+              <p><small className="text-muted">(Makes more than $200k annually for the past 2 years and will do so in this current year or have a net worth of $1M excluding your primary residence)</small></p>
               <div className="form-group">
-                <Field className="form-control" type="text" name="question8" />
+                <Field type="radio" name="question7" value="Low" className="m-2" />
+                <label htmlFor="question7">Yes</label>
+                <Field type="radio" name="question7" value="Moderate" className="m-2" />
+                <label htmlFor="question7">No</label>
               </div>
             </div>
-            <div>
+            <div className="mb-4">
+              <label htmlFor="question8">8. Can you show proof of funds?</label>
+              <div className="form-group">
+                <Field type="radio" name="question8" value="Low" className="m-2" />
+                <label htmlFor="question8">Yes</label>
+                <Field type="radio" name="question8" value="Moderate" className="m-2" />
+                <label htmlFor="question8">No</label>
+              </div>
+            </div>
+            <div className="mb-4">
               <label htmlFor="question9">9. How would you rate your risk tolerance?</label>
               <div className="form-group">
-                <Field type="radio" name="question9" value="Low" />
+                <Field type="radio" name="question9" value="Low" className="m-2" />
                 <label htmlFor="question9">Low</label>
-                <Field type="radio" name="question9" value="Moderate" />
+                <Field type="radio" name="question9" value="Moderate" className="m-2" />
                 <label htmlFor="question9">Moderate</label>
-                <Field type="radio" name="question9" value="High" />
+                <Field type="radio" name="question9" value="High" className="m-2" />
                 <label htmlFor="question9">High</label>
               </div>
             </div>
-            <div>
+            <div className="mb-4">
               <label htmlFor="question10">10. If you are out of the country, have you invested in the US real estate market in the past?
               </label>
               <div className="form-group">
-                <Field className="form-control" type="text" name="question10" />
+                <Field type="radio" name="question10" value="Low" className="m-2" />
+                <label htmlFor="question10">Yes</label>
+                <Field type="radio" name="question10" value="Moderate" className="m-2" />
+                <label htmlFor="question10">No</label>
               </div>
             </div>
-            <div>
+            <div className="mb-4">
               <label htmlFor="question11">11. Have you invested as a limited partner (LP) on a syndication deal in the past?</label>
               <div className="form-group">
-                <Field className="form-control" type="text" name="question11" />
+                <Field type="radio" name="question11" value="Low" className="m-2" />
+                <label htmlFor="question11">Yes</label>
+                <Field type="radio" name="question11" value="Moderate" className="m-2" />
+                <label htmlFor="question11">No</label>
               </div>
             </div>
-            <div>
+            <div className="mb-4">
               <label htmlFor="question12">12. Are you comfortable investing with other LPs or would you require to be the only LP in this investment?</label>
               <p><small className="text-muted">If you’re a substantial portion of the equity being invested in the deal, we would ask for you to commit non-refundable equity to create an alignment of interest to close.</small></p>
               <div className="form-group">
-                <Field className="form-control" type="text" name="question12" />
+                <Field type="radio" name="question12" value="Low" className="m-2" />
+                <label htmlFor="question12">Yes</label>
+                <Field type="radio" name="question12" value="Moderate" className="m-2" />
+                <label htmlFor="question12">No</label>
               </div>
             </div>
-            <div>
+            <div className="mb-4">
               <label htmlFor="question13">13. In a short paragraph please provide us with your investing experience.</label>
               <div className="form-group">
-                <Field className="form-control" type="text" name="question13" />
+                <Field className="form-control" type="text" name="question13" component="textarea" />
               </div>
             </div>
-            <div>
+            <div className="mb-4">
               <label htmlFor="question14">14. Is there anything else we should know about you and your investment goals?</label>
               <div className="form-group">
-                <Field className="form-control" type="text" name="question14" />
+                <Field className="form-control" type="text" name="question14" component="textarea"/>
               </div>
             </div>
           </div>
