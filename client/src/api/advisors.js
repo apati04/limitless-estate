5799f99e754af6c5d8f8d9465d93101d4aa5a4cf
Andrew Patipaksiri <apax714@gmail.com>
2018-03-10 10:57:13 -0800

    add data for popover

---
+++ client/src/api/advisors.js
@@ -0,0 +1,30 @@
+/** Listing keys here to check for duplicates. will need to refactor later
+ * Keys: MB, DK, JD, HL, OC
+ */
+module.exports = {
+  MB: {
+    name: 'Michael Blank, Advisor',
+    content:
+      'Mr. Blank has personally raised more than $2.6M for 35 residential and multifamily projects valued at over $10M.  His asset management partners control over $58 million in performing multifamily assets all over the United States.  In addition to his own investing activities, Mr. Blank provides training and consulting services (www.TheMichaelBlank.com) and has helped students acquire over 700 units valued in excess of $15,000,000.'
+  },
+  DK: {
+    name: 'Drew Kniffin, Advisor',
+    content:
+      'Mr. Kniffin coaches and invests in multifamily real estate and owns more than 350 units and has completed more than 10 deals.  Mr. Kniffin works with Michael Blank in his training and consulting services.'
+  },
+  JD: {
+    name: 'Jamie Douraghy, Advisor',
+    content:
+      'Mr. Douraghy is an executive coach and a team facilitator.  He is a Gallups Strengths Finder, Why certified coach and Founder of several successful companies.'
+  },
+  HL: {
+    name: 'Huasha Liu, Advisor',
+    content:
+      'Ms. Lui has been a successful real estate investor for over 20 years and has completed multiple transactions, both in residential and commercial properties.  She has a deep understanding of what may affect the real estate market such as supply and demand, state of the economy, demographic shift, cultural change, and technology advancement.'
+  },
+  OC: {
+    name: 'Old Capital Lending, Mortgage Broker & Advisor',
+    content:
+      'Since 1998, Old Capital has been a premier provider of commercial Capital solutions and advisory services for commercial real estate investors.'
+  }
+};
