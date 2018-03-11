e7f48a940c79001cd6161057184e52f3086b727c
Andrew Patipaksiri <apax714@gmail.com>
2018-03-11 00:00:35 -0800

    fix popover

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
-import $ from 'jquery';
-import 'bootstrap';
+import * as $ from 'jquery';
+import Popper from 'popper.js';
 import 'bootstrap-material-design/dist/css/bootstrap-material-design.min.css';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import './style/styles.css';
@@ -8,12 +8,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import { unregister } from './registerServiceWorker';
-
+window.jQuery = window.$ = $;
+window.Popper = Popper;
+require('bootstrap');
+require('bootstrap-material-design');
+$('body').bootstrapMaterialDesign();
 $(function() {
-  $('[data-toggle="popover"]').popover();
-  $('.popover-dismiss').popover({
+  $('[data-toggle="popover"]').popover({
+    container: 'body',
     trigger: 'focus'
   });
 });
+
 ReactDOM.render(<App />, document.getElementById('root'));
 unregister();
