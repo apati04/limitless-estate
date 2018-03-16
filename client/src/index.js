e86d3428119731f3cc133983a141ca1581b3db68
Andrew Patipaksiri <apax714@gmail.com>
2018-03-15 23:50:55 -0700

    form style edit

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,13 @@
-import 'bootstrap';
-import 'bootstrap-material-design/dist/css/bootstrap-material-design.min.css';
-import 'mapbox-gl/dist/mapbox-gl.css';
-import './style/styles.css';
-import 'mapbox-gl/dist/mapbox-gl';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import { unregister } from './registerServiceWorker';
+import "bootstrap";
+import "bootstrap-material-design/dist/css/bootstrap-material-design.min.css";
+import "mapbox-gl/dist/mapbox-gl.css";
+import "./style/styles.css";
+import "mapbox-gl/dist/mapbox-gl";
+import Popper from "popper.js";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import { unregister } from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 unregister();
--- client/src/pages/Questionnaire.js
+++ client/src/pages/Questionnaire.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import QuestionnaireForm from '../components/Forms/QuestionnaireForm';
-import PageHeader from '../components/PageHeader';
+import React from "react";
+import QuestionnaireForm from "../components/Forms/QuestionnaireForm";
+import PageHeader from "../components/PageHeader";
 
 const Questionnaire = () => {
   return (
@@ -8,14 +8,13 @@ const Questionnaire = () => {
       <div>
         <div className="container">
           <div className="row">
-            <div className="card">
+            <div className="card m-4">
               <div className="bg-dark">
-
-                <h1 className="p-5 card-header text-center text-white">Questionnaire</h1>
+                <h1 className="p-5 card-header text-center text-white">
+                  Questionnaire
+                </h1>
               </div>
-
-              <div className="card-body">
-           
+              <div className="card-body m-4">
                 <QuestionnaireForm />
               </div>
             </div>
@@ -24,6 +23,6 @@ const Questionnaire = () => {
       </div>
     </div>
   );
-}
+};
 
 export default Questionnaire;
