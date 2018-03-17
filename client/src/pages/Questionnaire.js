8714579c129cfc39f2c7803fc542e6abc329a651
Andrew Patipaksiri <apax714@gmail.com>
2018-03-17 03:59:56 -0700

    asf

--- client/src/pages/Questionnaire.js
+++ client/src/pages/Questionnaire.js
@@ -1,6 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-=======
 import {
   Card,
   CardImage,
@@ -9,28 +7,12 @@ import {
   CardText,
   Button
 } from "mdbreact";
->>>>>>> 3728fc05cf8c5dc1a709a3da74c0b1b8fe12dba7
 import QuestionnaireForm from "../components/Forms/QuestionnaireForm";
 import PageHeader from "../components/PageHeader";
 
 const Questionnaire = () => {
   return (
     <div>
-<<<<<<< HEAD
-      <div>
-        <div className="container">
-          <div className="row">
-            <div className="card">
-              <div className="bg-dark">
-                <h1 className="p-5 card-header text-center text-white">
-                  Questionnaire
-                </h1>
-              </div>
-
-              <div className="card-body">
-                <QuestionnaireForm />
-              </div>
-=======
       <div className="container d-flex justify-content-center align-items-center">
         <Card style={{ margin: "50px" }} className="container" cascade wide>
           <CardImage tag="div">
@@ -42,7 +24,6 @@ const Questionnaire = () => {
                 Please fill out the questions below to let us know more about
                 you!
               </p>
->>>>>>> 3728fc05cf8c5dc1a709a3da74c0b1b8fe12dba7
             </div>
           </CardImage>
           <CardBody>
