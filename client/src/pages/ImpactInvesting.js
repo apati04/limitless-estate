b3cc32778d33ba608f5766aa85cb1aa30364d9ac
Andrew Patipaksiri <apax714@gmail.com>
2018-03-20 13:16:36 -0700

    content template complete

--- client/src/pages/ImpactInvesting.js
+++ client/src/pages/ImpactInvesting.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
-
+import PostTemplate from '../components/templates/PostTemplate';
+const postData = {
+  title: 'post title',
+  text: [<div>asdfasdfasdf</div>, <h1>asdfasdfasdfasdf</h1>],
+  image: 'https://i.imgur.com/2A3IGvX.jpg'
+};
 export default class ImpactInvesting extends Component {
   render() {
-    return <div>Impact Investing</div>;
+    return (
+      <div>
+        <PostTemplate
+          headerImage="https://i.imgur.com/YXieUK4.jpg"
+          overlayColor="hm-purple-slight"
+          post={postData}
+        />
+      </div>
+    );
   }
 }
