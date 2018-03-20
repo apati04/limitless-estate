f56dbdf1bf38949c97dc2d205933d5240677c5f1
Andrew Patipaksiri <apax714@gmail.com>
2018-03-20 13:16:36 -0700

    content template complete

--- client/src/components/templates/PostTemplate.js
+++ client/src/components/templates/PostTemplate.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import { View } from 'mdbreact';
+import { View, Mask } from 'mdbreact';
 
-export default ({overlayColor, headerImage, pattern, overlayText, post}) => {
+export default ({
+  overlayColor,
+  headerImage,
+  pattern = '',
+  overlayText,
+  post = ''
+}) => {
   return (
     <section>
-      <View className={overlayColor}
-        src={headerImage}
-        alt=""
-      />
-      <Mask className={`flex-center ${pattern}`}>{overlayText}</Mask>
-
+      <View
+        style={{
+          height: '400px',
+          backgroundImage: `url(${headerImage})`,
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: '50% 50%'
+        }}
+        className={overlayColor}
+      >
+        <Mask className={`flex-center ${pattern}`}>{overlayText}</Mask>
+      </View>
       <div className="container d-flex justify-content-around py-3">
         <div className="p-1 col-lg-8 col-md-12">
           <div className="blog-post">
-            <h2 className="blog-post-title mb-2">
-              {post.title}
-            </h2>
-            <div className="border-top">
-              {post.text}
-            </div>
+            <h2 className="blog-post-title mb-2">{post.title}</h2>
+            <div className="border-top">{post.text}</div>
           </div>
         </div>
         <div className="m-0 p-0 col-lg-4">
@@ -28,10 +36,10 @@ export default ({overlayColor, headerImage, pattern, overlayText, post}) => {
             width="320px"
             height="320px"
             alt="LimitLess"
-            className="border border-bottom-0 border-secondary p-0 about-img img-fluid rounded-circle d-none d-lg-block"
+            className="border about-img img-fluid rounded-circle d-none d-lg-block"
           />
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
