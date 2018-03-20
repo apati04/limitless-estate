e6a2e634b7c5367249ac10a1e644c25e9b3d2a5b
Andrew Patipaksiri <apax714@gmail.com>
2018-03-20 16:35:27 -0700

    header

--- client/src/components/landing_section/Header.js
+++ client/src/components/landing_section/Header.js
@@ -1,4 +1,4 @@
-import '../../style/blog.css';
+// import '../../style/blog.css';
 import React, { Component } from 'react';
 import { View, Mask, Button } from 'mdbreact';
 import { NavLink } from 'react-router-dom';
@@ -19,32 +19,38 @@ export default class HeaderSection extends Component {
   }
   render() {
     return (
-      <View className="hm-blue-slight">
+      <View className="hm-stylish-slight">
         <img
           className="img-fluid w-100"
-          src="https://static1.squarespace.com/static/59de45142994cabf2c18b636/t/59f282b8652deaab9521c562/1509065425627/ebs-home.png"
+          src="https://scontent-ort2-1.cdninstagram.com/vp/7eab7610543caa80a5a82f3d41fc860f/5B313D26/t51.2885-15/e35/28763645_775007869372653_7749070619253145600_n.jpg"
           alt=""
         />
         <Mask className="d-flex full-bg-img flex-center ">
-          <div className="d-flex container justify-content-between text-left white-text wow fadeInUp">
-            <div>
-              <h3>
-                Our vision at Limitless Estates is to provide A class living to
-                lower income housing by putting our residents first and
-                instilling a sense of community while inspiring others to do the
-                same.
-              </h3>
-            </div>
-            <div>
-              <h3>
-                Our mission is to positively impact the lives of the people in
-                our local neighborhoods through impact investing (hyperlink to
-                our article on impact investing) while achieving double digit
-                returns for our investors.
-              </h3>
-              <Button className="text-right" color="mdb">
-                Make An Impact
-              </Button>
+          <div className="d-flex container justify-content-around p-0 text-left white-text wow fadeInUp">
+            <div className="text-center">
+              <h2 className="mb-4">
+                Our <strong>vision</strong> at Limitless Estates is to provide A
+                class living to lower income housing by putting our residents
+                first and instilling a sense of community while inspiring others
+                to do the same.
+              </h2>
+
+              <h2 className="mt-4 text-center">
+                Our <strong>mission</strong> is to positively impact the lives
+                of the people in our local neighborhoods through{' '}
+                <NavLink
+                  style={{ color: 'bluegrey' }}
+                  to="/resources/impactinvesting"
+                >
+                  impact investing
+                </NavLink>{' '}
+                while achieving double digit returns for our investors.
+              </h2>
+              <div className="text-center">
+                <Button className="text-right" size="lg" color="primary">
+                  Make An Impact
+                </Button>
+              </div>
             </div>
           </div>
         </Mask>
@@ -53,5 +59,11 @@ export default class HeaderSection extends Component {
   }
 }
 // https://images.pexels.com/photos/936713/pexels-photo-936713.jpeg
-// https://static1.squarespace.com/static/59de45142994cabf2c18b636/t/59f282b8652deaab9521c562/1509065425627/ebs-home.png
-// https://images.pexels.com/photos/936715/pexels-photo-936715.jpeg
+//
+
+// https://static1.squarespace.com/static/59de45142994cabf2c18b636/t/59f282b8652deaab9521c562/1509065425627/ebs-home.png?format=1920w
+/*
+https://images.pexels.com/photos/7277/sunset-water-clouds-lake.jpg?w=1260&h=750&dpr=2&auto=compress&cs=tinysrgb
+
+
+*/
