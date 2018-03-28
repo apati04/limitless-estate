a5ba727e0493e3b28f9507cd270cf8c4321e71b8
Andrew Patipaksiri <apax714@gmail.com>
2018-03-28 13:42:56 -0700

    styling

--- client/src/components/LandingParallax.js
+++ client/src/components/LandingParallax.js
@@ -2,19 +2,13 @@ import React from 'react';
 
 const LandingParallax = () => {
   return (
-    <div>
-      <section
-        style={{ height: 10 + 'rem' }}
-        id="landingParallax"
-        className="p-5"
-      >
-        <div className="dark-overlay d-flex justify-content-center align-items-center">
-          <h1 className="h1-responsive text-white">
-            Reasons to invest in multifamily...
-          </h1>
-        </div>
-      </section>
-    </div>
+    <section id="landingParallax" className="p-5">
+      <div className="dark-overlay d-flex justify-content-center align-items-center">
+        <h1 className="h1-responsive text-white">
+          Reasons to invest in multifamily...
+        </h1>
+      </div>
+    </section>
   );
 };
 export default LandingParallax;
