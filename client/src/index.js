9c009f11f8323915bd714c2a76c4ad042ff7ff1b
Andrew Patipaksiri <apax714@gmail.com>
2018-03-29 23:43:34 -0700

    wip

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'mdbreact/docs/css/mdb.min.css';
 import './style/styles.css';
-
 import 'mapbox-gl/dist/mapbox-gl.css';
 import 'mapbox-gl/dist/mapbox-gl';
 import './style/font.css';
