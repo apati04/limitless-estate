0f6b9761f75e61e93ee342d9dcdbd274f7d342b7
Andrew Patipaksiri <apax714@gmail.com>
2018-03-29 22:42:17 -0700

    update

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -68,6 +68,7 @@ class App extends Component {
               <Route exact path="/" component={LandingPage} />
             </ScrollToTop>
           </Switch>
+
           <Footer />
         </div>
       </BrowserRouter>
