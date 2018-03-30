7b78a6fbb59587f2c9c57fc43f9089f935d22233
Andrew Patipaksiri <apax714@gmail.com>
2018-03-30 13:17:44 -0700

    changes wip

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'mdbreact/docs/css/mdb.min.css';
