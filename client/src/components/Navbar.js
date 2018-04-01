61be7707124cc1e347085d727ac7148a03cae1f2
isaac1104 <isaac-kwon@hotmail.com>
2018-03-31 20:45:24 -0700

    link added to logo

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import {
   Navbar,
   NavbarBrand,
@@ -45,13 +45,15 @@ class Navigation extends Component {
         className="blue p-2 m-0"
       >
         <Container>
-          <NavbarBrand to="/">
-            <img
-              height="50"
-              src="https://i.imgur.com/6TpCmWV.png"
-              className="d-inline-block ml-auto"
-              alt=""
-            />
+          <NavbarBrand>
+            <Link to="/">
+              <img
+                height="50"
+                src="https://i.imgur.com/6TpCmWV.png"
+                className="d-inline-block ml-auto"
+                alt=""
+              />
+            </Link>
           </NavbarBrand>
           {!this.state.isWideEnough && <NavbarToggler onClick={this.onClick} />}
 
