7ab0434efe861bb81061920d77bd0920c86457de
Andrew Patipaksiri <apax714@gmail.com>
2018-04-06 23:32:34 -0700

    display columns

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,13 @@
-import './style/styles.css';
-import './style/normalize';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './style/normalize.css';
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'mdbreact/docs/css/mdb.min.css';
+import './style/styles.css';
 
 import App from './components/App';
 import { unregister } from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
 unregister();
