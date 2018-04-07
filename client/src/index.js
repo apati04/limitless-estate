20886793a0e37b134bd554981ed1fd0fedeb39bb
Andrew Patipaksiri <apax714@gmail.com>
2018-04-06 21:39:49 -0700

    fix double scrollbars on safari

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
+import './style/styles.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'mdbreact/docs/css/mdb.min.css';
-import './style/styles.css';
 
 import App from './components/App';
 import { unregister } from './registerServiceWorker';
