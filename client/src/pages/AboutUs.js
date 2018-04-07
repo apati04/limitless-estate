ad8944a38b983daf2a0f634694bd685ee2a542d5
Andrew Patipaksiri <apax714@gmail.com>
2018-04-06 20:36:55 -0700

    style about

--- client/src/pages/AboutUs.js
+++ client/src/pages/AboutUs.js
@@ -18,69 +18,77 @@ class AboutUs extends Component {
             </div>
           </div>
 
-          <div className="row flex-center text-center">
-            <h3>Kyle Mitchell</h3>
-            <p>
-              My passion is in helping others reach their goals in all areas of
-              life by doing things the right way and creating long lasting
-              relationships based on trust and clarity. I have experience with
-              operations management; staff development; revenue generation;
-              expense management; food & beverage operations; budgeting; sales;
-              capital improvement projects; single family homes; buying and
-              selling real estate; and operating boutique fitness studios.
-            </p>
-            <p>
-              Real Estate Investor: I have been investing in income producing
-              Real Estate since 2015, I currently own 10 single family homes in
-              4 different markets (California, Illinois, Ohio & Arkansas)
-              totaling to $1.2 million. My goal for 2018 is to own 160 apartment
-              units and my 5-year goal is to own 1,000 apartment units.
-            </p>
-            <p>
-              Club Pilates: I am a Club Pilates franchise owner in Southern
-              California. Club Pilates was founded in San Diego in 2007 and has
-              expanded across the nation, bringing Pilates to hundreds of
-              thousands of people. In 2017, Club Pilates served up over 2.2
-              million workouts to their dedicated members. We believe that
-              Pilates is the path to a fuller, more satisfying physical
-              existence. We believe that being in control of your body helps you
-              to be in control of your life. And best of all, we believe that
-              you can start anytime! In 2016, Club Pilates was recognized as #31
-              in Entrepreneur Magazine’s Top New Franchise and most recently in
-              2017 as #361 in the Entrepreneur Franchise 500®.
-            </p>
-            <p>
-              Real Estate Agent: I am a licensed Realtor in California and have
-              been buying and selling Real Estate since 2015. One of the ways I
-              love to give back is by donating 20% of my commissions to the
-              local charities of my client’s choice. My goal is to one day be
-              able to donate 100% of my commissions back to local charities.
-            </p>
-            <p>
-              American Golf: I am a Regional Manager for American Golf and have
-              15+ years of experience in operations management including
-              overseeing more than $18 million in revenue and 225+ team members.
-              I began working for American Golf in 1997 and worked my way up
-              from a cashier in high school to General Manager by the age of 21.
-              I also serve on the board of Directors for the First Tee of
-              Greater Pasadena where our mission is to impact the lives of young
-              people from all walks of life by reinforcing values like
-              integrity, respect and perseverance through educational programs
-              that build character, instill life-enhancing values and promote
-              healthy choices through the game of golf.
-            </p>
-            <p>
-              Other: I played golf Professionally for a few years around
-              California before working full-time for American Golf. I am a dog
-              owner & lover, I enjoy cooking, traveling, fitness & sports. I
-              currently live in Southern California and the thing I am most
-              looking forward to this year is planning my wedding with my
-              fiancé. The most important thing I have learned over the last 12
-              months is how important it is to spend time and care for your
-              family, nothing is more important than family. I am a big believer
-              in a positive mindset, continued education, improving yourself day
-              by day and taking massive action.
-            </p>
+          <div className="row  flex-center text-align-center text-center">
+            <div className="col-10">
+              <h3>Kyle Mitchell</h3>
+              <h6>President</h6>
+
+              <h3 className="mt-2">
+                My passion is in helping others reach their goals in all areas
+                of life by doing things the right way and creating long lasting
+                relationships based on trust and clarity. I have experience with
+                operations management; staff development; revenue generation;
+                expense management; food & beverage operations; budgeting;
+                sales; capital improvement projects; single family homes; buying
+                and selling real estate; and operating boutique fitness studios.
+              </h3>
+
+              <p className="lead pt-4">
+                Real Estate Investor: I have been investing in income producing
+                Real Estate since 2015, I currently own 10 single family homes
+                in 4 different markets (California, Illinois, Ohio & Arkansas)
+                totaling to $1.2 million. My goal for 2018 is to own 160
+                apartment units and my 5-year goal is to own 1,000 apartment
+                units.
+              </p>
+
+              <p className="lead pt-4">
+                Club Pilates: I am a Club Pilates franchise owner in Southern
+                California. Club Pilates was founded in San Diego in 2007 and
+                has expanded across the nation, bringing Pilates to hundreds of
+                thousands of people. In 2017, Club Pilates served up over 2.2
+                million workouts to their dedicated members. We believe that
+                Pilates is the path to a fuller, more satisfying physical
+                existence. We believe that being in control of your body helps
+                you to be in control of your life. And best of all, we believe
+                that you can start anytime! In 2016, Club Pilates was recognized
+                as #31 in Entrepreneur Magazine’s Top New Franchise and most
+                recently in 2017 as #361 in the Entrepreneur Franchise 500®.
+              </p>
+              <p className="lead pt-4">
+                Real Estate Agent: I am a licensed Realtor in California and
+                have been buying and selling Real Estate since 2015. One of the
+                ways I love to give back is by donating 20% of my commissions to
+                the local charities of my client’s choice. My goal is to one day
+                be able to donate 100% of my commissions back to local
+                charities.
+              </p>
+              <p className="lead pt-4">
+                American Golf: I am a Regional Manager for American Golf and
+                have 15+ years of experience in operations management including
+                overseeing more than $18 million in revenue and 225+ team
+                members. I began working for American Golf in 1997 and worked my
+                way up from a cashier in high school to General Manager by the
+                age of 21. I also serve on the board of Directors for the First
+                Tee of Greater Pasadena where our mission is to impact the lives
+                of young people from all walks of life by reinforcing values
+                like integrity, respect and perseverance through educational
+                programs that build character, instill life-enhancing values and
+                promote healthy choices through the game of golf.
+              </p>
+              <p className="lead pt-4">
+                Other: I played golf Professionally for a few years around
+                California before working full-time for American Golf. I am a
+                dog owner & lover, I enjoy cooking, traveling, fitness & sports.
+                I currently live in Southern California and the thing I am most
+                looking forward to this year is planning my wedding with my
+                fiancé. The most important thing I have learned over the last 12
+                months is how important it is to spend time and care for your
+                family, nothing is more important than family. I am a big
+                believer in a positive mindset, continued education, improving
+                yourself day by day and taking massive action.
+              </p>
+            </div>
           </div>
           <div>
             <div className="no-flex testimonial-carousel">
