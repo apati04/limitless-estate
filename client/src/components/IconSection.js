bca5ef245a008668d040bdd90dc75c88548db638
Andrew Patipaksiri <apax714@gmail.com>
2018-04-12 14:08:52 -0700

    build2

--- client/src/components/IconSection.js
+++ client/src/components/IconSection.js
@@ -3,33 +3,37 @@ import IconTemplate from './IconTemplate';
 
 const IconSection = () => {
   return (
-    <section
-      style={{ minHeight: 20 + 'em' }}
-      id="landingIcons"
-      className="p-4 d-flex container flex-column flex-md-row justify-content-between align-items-center"
-    >
-      <IconTemplate
-        icon="fas fa-child"
-        title="Impacting Lives"
-        fontSize={3 + 'em'}
-      />
+    <section id="landingIcons">
+      <div className="container p-2">
+        <div
+          style={{ minHeight: 20 + 'em' }}
+          className="row flex-column flex-md-row justify-content-around align-items-center flex-nowrap"
+        >
+          <IconTemplate
+            icon="fas fa-child"
+            title="Impacting Lives"
+            fontSize={3 + 'em'}
+          />
 
-      <IconTemplate
-        icon="fas fa-users"
-        title="Community Development"
-        fontSize={3 + 'em'}
-      />
-      <IconTemplate
-        icon="fab fa-medapps"
-        title="Inspire Others"
-        fontSize={3 + 'em'}
-      />
+          <IconTemplate
+            icon="fas fa-users"
+            title="Community Development"
+            fontSize={3 + 'em'}
+          />
 
-      <IconTemplate
-        icon="fas fa-trophy"
-        title="Create Win Win Win Scenarios"
-        fontSize={3 + 'em'}
-      />
+          <IconTemplate
+            icon="fab fa-medapps"
+            title="Inspire Others"
+            fontSize={3 + 'em'}
+          />
+
+          <IconTemplate
+            icon="fas fa-trophy"
+            title="Create Win Win Win Scenarios"
+            fontSize={3 + 'em'}
+          />
+        </div>
+      </div>
     </section>
   );
 };
--- client/src/components/IconTemplate.js
+++ client/src/components/IconTemplate.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 const IconTemplate = ({ icon, title, fontSize }) => {
   return (
-    <div className="text-center">
-      <i
-        className={`icon-landing ${icon} mb-2`}
-        style={{ fontSize: `${fontSize}` }}
-      />
-      <div>
-        <div className="h5-responsive mt-2">{title}</div>
+    <div>
+      <div className="col-12 text-center m-1">
+        <i
+          className={`icon-landing ${icon} mb-2`}
+          style={{ fontSize: `${fontSize}` }}
+        />
+        <div>
+          <div className="h5-responsive">{title}</div>
+        </div>
       </div>
     </div>
   );
