a23ba88b90dcc611a250c070c739a53fa30040dd
Andrew Patipaksiri <apax714@gmail.com>
2018-05-01 17:20:43 -0700

    edit timestamps

--- client/src/components/FormatDate/FormatDate.js
+++ client/src/components/FormatDate/FormatDate.js
@@ -1,14 +1,11 @@
 import React, { Component, Fragment } from 'react';
 import Moment from 'react-moment';
 
-export default props => {
-  const meetupDate = new Date(1525829400000);
+export default ({ meetupTimeStamp }) => {
+  const meetupDate = new Date(meetupTimeStamp);
   return (
     <Fragment>
-      <Moment format="dddd, MMMM Do YYYY, h:mm a">{meetupDate}</Moment> to{' '}
-      <Moment add={{ hours: 2 }} format="h:mm a">
-        {meetupDate}
-      </Moment>
+      <Moment format="MMMM Do YYYY">{meetupDate}</Moment>
     </Fragment>
   );
 };
--- client/src/pages/Events/MeetUps.js
+++ client/src/pages/Events/MeetUps.js
@@ -19,7 +19,11 @@ class MeetUps extends Component {
   renderMeetups = () => {
     const { meetups, max } = this.state;
     if (Array.isArray(meetups)) {
-      return meetups.map((el, index) => <div key={index}>{el.local_date}</div>);
+      return meetups.map((el, index) => (
+        <div key={index}>
+          <FormatDate meetupTimeStamp={el.time} />
+        </div>
+      ));
     }
     return <div>Loading...</div>;
   };
@@ -31,7 +35,7 @@ class MeetUps extends Component {
     return (
       <div>
         <h2>{meetupName || ''}</h2>
-        <FormatDate />
+
         {this.renderMeetups()}
       </div>
     );
@@ -42,3 +46,12 @@ export default MeetUps;
 /**
  * <time datetime="1525829400000"><span class="eventTimeDisplay-startDate"><span>Tuesday, May 8, 2018</span><br><span class="eventTimeDisplay-startDate-time"><span>6:30 PM</span></span></span><span class="eventTimeDisplay-endDate"> <span>to <span class="eventTimeDisplay-endDate-partialTime"><span>8:30 PM</span></span></span></span></time>
  */
+
+/**
+  *     <Fragment>
+      <Moment format="dddd, MMMM Do YYYY, h:mm a">{meetupDate}</Moment> to{' '}
+      <Moment add={{ hours: 2 }} format="h:mm a">
+        {meetupDate}
+      </Moment>
+    </Fragment>
+  */
