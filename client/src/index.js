e16cd1dfe9c3d007cf47589ce63363254d75015a
Andrew Patipaksiri <apax714@gmail.com>
2018-07-18 00:58:55 -0700

    gallery

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style/dist/css/mdb.min.css';
 import './style/normalize.css';
