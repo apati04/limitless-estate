12b4a57730e0c23c8ce9c37e36913dba71364181
Andrew Patipaksiri <apax714@gmail.com>
2018-09-03 20:47:20 -0700

    subscribe component template

---
+++ client/src/components/MailChimp/Subscribe.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Subscribe extends Component {
+  onSubmit = e => {
+    // check if email is missing
+    // if exists, call method to sub
+  };
+  render() {
+    return <div />;
+  }
+}
+
+export default Subscribe;
