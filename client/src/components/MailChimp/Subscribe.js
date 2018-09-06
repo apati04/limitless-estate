4dea97b21577a6c26feb372ae5983d33d02ab7cc
Andrew Patipaksiri <apax714@gmail.com>
2018-09-05 17:25:27 -0700

    frontend form implementation

--- client/src/components/MailChimp/Subscribe.js
+++ client/src/components/MailChimp/Subscribe.js
@@ -13,26 +13,58 @@ import axios from 'axios';
 class Subscribe extends Component {
   state = {
     modal: false
-  }
+  };
   onSubmit = e => {
     // check if email is missing
     // if exists, call method to sub
   };
   render() {
+    const { values, errors, touched, isSubmitting } = this.props;
     return (
       <div>
         <Form>
-        <div className="row">
+          <div className="row">
             <div className="col-md-6">
-              <Field component="input" className="form-control" type="email" name="email" placeholder="Your Email Address" />
-              {touched.email && errors.email && (<small className="font-italic text-danger">{errors.email}</small>)}
+              <Field
+                component="input"
+                className="form-control"
+                type="email"
+                name="email"
+                placeholder="Your Email Address"
+              />
+              {touched.email &&
+                errors.email && (
+                  <small className="font-italic text-danger">
+                    {errors.email}
+                  </small>
+                )}
             </div>
-            <div></div>
-        </div>
+            <div className="col-md-6">
+              <button type="submit" className="btn btn-outline primary-color">
+                {' '}
+                Subscribe
+              </button>
+            </div>
+          </div>
         </Form>
       </div>
     );
   }
 }
 
-export default Subscribe;
+export default withFormik({
+  mapPropsToValues({ email = '' }) {
+    console.log('mapprops: ', email);
+    return { email };
+  },
+  validationSchema: Yup.object().shape({
+    email: Yup.string()
+      .email('Email is not valid')
+      .required('Required')
+  }),
+  async handleSubmit(values, { resetForm, setErrors, setSubmitting }) {
+    const subscribe = await axios.post('/api/mailchimp/subscribe', values);
+
+    console.log('handlesubmit: ', values);
+  }
+})(Subscribe);
