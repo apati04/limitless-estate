1d1af00ecc113cd9d5c53928b6f09762997ee939
Andrew Patipaksiri <apax714@gmail.com>
2018-12-11 14:37:25 -0800

    headline changes

--- client/src/components/PicCarousel.js
+++ client/src/components/PicCarousel.js
@@ -1,17 +1,17 @@
-import React, { Component } from "react";
-import { NavLink } from "react-router-dom";
-import { View, Mask } from "mdbreact";
-import Limitless from "./wistia/limitless";
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import { View, Mask } from 'mdbreact';
+import Limitless from './wistia/limitless';
 class PicCarousel extends Component {
   render() {
     const caption = (
-      <div className="light-overlay">
-        <Mask style={{ paddingTop: 60 + "px" }} className="container">
-          <div className="row justify-content-between align-items-center">
-            <div className="col-6">
+      <div className='light-overlay'>
+        <Mask style={{ paddingTop: 60 + 'px' }} className='container'>
+          <div className='row justify-content-between align-items-center'>
+            <div className='col-6'>
               <h2
-                style={{ color: "white", marginBottom: 2 + "%" }}
-                className="text-left h4-responsive"
+                style={{ color: 'white', marginBottom: 2 + '%' }}
+                className='text-left h4-responsive'
               >
                 Our vision at Limitless Estates is to provide A-class living to
                 lower income housing by putting our residents first and
@@ -19,62 +19,62 @@ class PicCarousel extends Component {
                 same.
               </h2>
               <h4
-                style={{ color: "white", marginBottom: 2 + "%" }}
-                className="text-left h4-responsive"
+                style={{ color: 'white', marginBottom: 2 + '%' }}
+                className='text-left h4-responsive'
               >
                 Our mission is to positively impact the lives of the people in
-                our local neighborhoods through{" "}
+                our local neighborhoods through{' '}
                 <a
-                  className=" text-success font-italic font-weight-bold"
-                  href="/resources/impactinvesting"
+                  className=' text-success font-italic font-weight-bold'
+                  href='/resources/impactinvesting'
                 >
                   impact investing
-                </a>{" "}
+                </a>{' '}
                 while achieving double digit returns for our investors.
               </h4>
               <div
-                style={{ marginTop: 4 + "%" }}
-                className="d-none d-md-block text-center h4-responsive"
+                style={{ marginTop: 4 + '%' }}
+                className='d-none d-md-block text-center h4-responsive'
               >
                 <NavLink
-                  style={{ borderRadius: "5px", background: "#1847A1" }}
-                  className="btn"
-                  to="info/questionnaire"
+                  style={{ borderRadius: '5px', background: '#1847A1' }}
+                  className='btn'
+                  to='info/questionnaire'
                 >
-                  Get Started Here
+                  Investor Questionnaire
                 </NavLink>
               </div>
             </div>
-            <div className="col-6">
+            <div className='col-6'>
               <Limitless
-                wistiaSrc="https://fast.wistia.com/embed/medias/pueh6irs0z/swatch"
-                wistiaClass="wistia_embed wistia_async_pueh6irs0z videoFoam=true"
+                wistiaSrc='https://fast.wistia.com/embed/medias/pueh6irs0z/swatch'
+                wistiaClass='wistia_embed wistia_async_pueh6irs0z videoFoam=true'
               />
             </div>
           </div>
-          <div className="row justify-content-center align-items-center mt-4">
-            <div style={{ color: "white" }} className="mt-4 col-6">
+          <div className='row justify-content-center align-items-center mt-4'>
+            <div style={{ color: 'white' }} className='mt-4 col-6'>
               <Limitless
-                wistiaSrc="https://fast.wistia.com/embed/medias/mtdamm7u8i/swatch"
-                wistiaClass="wistia_embed wistia_async_mtdamm7u8i videoFoam=true"
+                wistiaSrc='https://fast.wistia.com/embed/medias/mtdamm7u8i/swatch'
+                wistiaClass='wistia_embed wistia_async_mtdamm7u8i videoFoam=true'
               />
             </div>
-            <div className="col-6 text-center text-justify-center text-md-center white-text">
-              <h4 className="text-left h4-responsive mt-2">
+            <div className='col-6 text-center text-justify-center text-md-center white-text'>
+              <h4 className='text-left h4-responsive mt-2'>
                 Did you know you can invest with your 401k/IRA? Watch the video
                 to find out more.
               </h4>
-              <h4 className="text-left h4-responsive mt-2">
+              <h4 className='text-left h4-responsive mt-2'>
                 Click the link below to connect with our preferred partners and
                 setup your self-directed IRA
               </h4>
               <NavLink
-                className="btn btn-primary"
-                to="/info/investing/ira401k"
-                size="md"
-                style={{ borderRadius: "5px" }}
+                className='btn btn-primary'
+                to='/info/investing/ira401k'
+                size='md'
+                style={{ borderRadius: '5px' }}
               >
-                Invest With your 401k/IRA
+                Preferred Partners
               </NavLink>
             </div>
           </div>
@@ -83,67 +83,67 @@ class PicCarousel extends Component {
     );
     const styles = {
       header: {
-        padding: "0",
-        background: "url(https://i.imgur.com/BI90AZ5.jpg?2) bottom no-repeat",
-        backgroundSize: "cover",
-        minHeight: 50 + "em"
+        padding: '0',
+        background: 'url(https://i.imgur.com/BI90AZ5.jpg?2) bottom no-repeat',
+        backgroundSize: 'cover',
+        minHeight: 50 + 'em'
       },
       mobileHeader: {
-        padding: "0",
-        background: "url(https://i.imgur.com/BI90AZ5.jpg?2) center no-repeat",
-        backgroundSize: "cover",
-        height: 25 + "em"
+        padding: '0',
+        background: 'url(https://i.imgur.com/BI90AZ5.jpg?2) center no-repeat',
+        backgroundSize: 'cover',
+        height: 25 + 'em'
       }
     };
     return (
       <section>
-        <div className="d-none d-md-block">
+        <div className='d-none d-md-block'>
           <View>
             <div style={styles.header} />
             <div>{caption}</div>
           </View>
         </div>
 
-        <div className="d-block d-md-none">
+        <div className='d-block d-md-none'>
           <View>
             <div style={styles.mobileHeader} />
-            <div className="light-overlay">
-              <Mask className="black-light container">
-                <div className="row justify-content-center flex-center">
-                  <div className="col">
-                    <div className="p-2 text-center text-white">
+            <div className='light-overlay'>
+              <Mask className='black-light container'>
+                <div className='row justify-content-center flex-center'>
+                  <div className='col'>
+                    <div className='p-2 text-center text-white'>
                       Our vision at Limitless Estates is to provide A-class
                       living to lower income housing by putting our residents
                       first and instilling a sense of community while inspiring
                       others to do the same.
                     </div>
-                    <div className="p-2 text-center text-white">
+                    <div className='p-2 text-center text-white'>
                       Our mission is to positively impact the lives of the
-                      people in our local neighborhoods through{" "}
+                      people in our local neighborhoods through{' '}
                       <NavLink
-                        to="/resources/impactinvesting"
-                        className=" text-success font-italic font-weight-bold"
+                        to='/resources/impactinvesting'
+                        className=' text-success font-italic font-weight-bold'
                       >
                         impact investing
-                      </NavLink>{" "}
+                      </NavLink>{' '}
                       while achieving double digit returns for our investors.
                     </div>
-                    <div className="p-2 text-center">
+                    <div className='p-2 text-center'>
                       <a
-                        href="https://ehypauluugknktzawdwva-free.10to8.com/"
-                        rel="noopener noreferrer"
-                        target="_blank"
-                        style={{ whiteSpace: "nowrap" }}
-                        className="btn btn-sm btn-primary btn-rounded align-items-center"
+                        href='https://ehypauluugknktzawdwva-free.10to8.com/'
+                        rel='noopener noreferrer'
+                        target='_blank'
+                        style={{ whiteSpace: 'nowrap' }}
+                        className='btn btn-sm btn-primary btn-rounded align-items-center'
                       >
-                        Schedule a Call{" "}
+                        Schedule a Call{' '}
                         <i
-                          className="fa fa-volume-control-phone"
-                          aria-hidden="true"
+                          className='fa fa-volume-control-phone'
+                          aria-hidden='true'
                           style={{
-                            fontSize: "13px",
-                            paddingTop: "2px",
-                            paddingLeft: "8px"
+                            fontSize: '13px',
+                            paddingTop: '2px',
+                            paddingLeft: '8px'
                           }}
                         />
                       </a>
@@ -153,7 +153,7 @@ class PicCarousel extends Component {
               </Mask>
             </div>
           </View>
-          <div className="rgba-stylish-slight p-1">
+          <div className='rgba-stylish-slight p-1'>
             <Limitless />
           </div>
         </div>
--- client/src/pages/Questionnaire.js
+++ client/src/pages/Questionnaire.js
@@ -11,23 +11,23 @@ const Questionnaire = () => {
           backgroundSize: 'cover',
           backgroundAttachment: 'fixed'
         }}
-        data-wow-delay="1s"
-        className="unique-color  animated fadeIn hm-black-light full-bg-img"
+        data-wow-delay='1s'
+        className='unique-color  animated fadeIn hm-black-light full-bg-img'
       >
-        <div className="container-fluid">
-          <div className="row justify-content-center py-5">
-            <div className="col-12 col-md-10 col-lg-8">
-              <Card className="animated fadeInUp z-depth-4" narrow>
-                <div className="view gradient-card-header blue-gradient text-white text-center p-2">
-                  <h2>Investor Qualifier Questionnaire</h2>
+        <div className='container-fluid'>
+          <div className='row justify-content-center py-5'>
+            <div className='col-12 col-md-10 col-lg-8'>
+              <Card className='animated fadeInUp z-depth-4' narrow>
+                <div className='view gradient-card-header blue-gradient text-white text-center p-2'>
+                  <h2>Investor Questionnaire</h2>
                   <p>
                     Please fill out the questions below to let us know more
                     about you!
                   </p>
                 </div>
                 <CardBody>
-                  <div className="row justify-content-md-center justify-content-between">
-                    <div className="col-11">
+                  <div className='row justify-content-md-center justify-content-between'>
+                    <div className='col-11'>
                       <QuestionnaireForm />
                     </div>
                   </div>
