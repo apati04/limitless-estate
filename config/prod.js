9a1f36e33845e878db3543c8789f7f87a1185963
Andrew Patipaksiri <apax714@gmail.com>
2019-03-01 00:41:18 -0800

    fix keys

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,7 @@ module.exports = {
   userGmail: process.env.USER_GMAIL,
   passGmail: process.env.PASS_GMAIL,
   meetupApiKey: process.env.MEETUP_API_KEY,
-  mailChimpApiKey: process.env.MAILCHIMP_API_KEY
+  mailChimpApiKey: process.env.MAILCHIMP_API_KEY,
+  podcastApiKey: process.env.PD_API,
+  podcastIdKey: process.env.PD_ID
 };
